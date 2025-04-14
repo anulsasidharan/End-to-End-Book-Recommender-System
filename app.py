f1c3ff1aebecced9b47e22cc229974ee8211350c
anulsasidharan <anulsasidharan@gmail.com>
2025-04-14 08:34:55 -0400

    Added custome logger

---
+++ app.py
@@ -0,0 +1,10 @@
+from books_recommender.exception.exception_handler import AppException
+from books_recommender.logger.log import logging
+import sys
+
+
+try:
+    a=30/0
+except Exception as e:
+    logging.info(e)
+    raise AppException(e,sys) from e
