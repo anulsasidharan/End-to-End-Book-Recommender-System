d1f6685a4351d83e571800b07a57cd6179c747be
anulsasidharan <anulsasidharan@gmail.com>
2025-04-15 14:17:40 -0400

    Code Correction and data ingestion sucess.

--- books_recommender/config/configuration.py
+++ books_recommender/config/configuration.py
@@ -8,16 +8,17 @@ from books_recommender.constant import *
 
 
 class AppConfiguration:
-    def__init__(self, config_file_path:str = CONFIG_FILE_PATH)
-    try:
-        self.config_info = read_yaml_file(file_path=config_file_path)
-    except Exception as e:
-        raise AppConfiguration(e, sys) from e
+    def __init__(self, config_file_path: str = CONFIG_FILE_PATH):
+        try:
+            self.configs_info = read_yaml_file(file_path=config_file_path)
+        except Exception as e:
+            raise AppException(e, sys) from e
+    
     
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         try:
-            data_ingestion_config = self.config_info['data_ingestion_config']
-            artifacts_dir = self.config_info['artifacts_config']['artifacts_dir']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
             dataset_dir = data_ingestion_config['dataset_dir']
 
             ingested_data_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config['ingested_dir'])
--- books_recommender/entity/config_entity.py
+++ books_recommender/entity/config_entity.py
@@ -2,7 +2,7 @@
 from collections import namedtuple
 
 DataIngestionConfig = namedtuple("DatasetConfig", ["dataset_download_url",
-                                                   "raw_data_dir,"
+                                                   "raw_data_dir",
                                                    "ingested_dir"])
 
 DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
